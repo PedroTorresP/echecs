6030c51743ca7fdd0fe212be0b7b32d3a3f9b18e
PedroTorresP <pedro-gabriel.torres-pacheco@etu.hesge.ch>
2018-03-01 14:23:43 +0100

    ajout projet

---
+++ Echecs/src/echecs/Main.java
@@ -0,0 +1,7 @@
+package echecs;
+public class Main {
+
+    public static void main(String[] args) {
+        new vue.FrmMain().setVisible(true);
+    }
+}
---
+++ Echecs/src/elo/Joueur.java
@@ -0,0 +1,28 @@
+package elo;
+
+public class Joueur implements Comparable {
+    
+    private String nom, prenom, nationalite;
+    private int points;
+    
+    public Joueur (String nom, String prenom, String nationalite, int points) { this.nom=nom; this.prenom=prenom; this.nationalite=nationalite; this.points=points; }
+    
+    public int getPoints() {return points;}
+    
+    public String getNom() {return nom;}
+    
+    /** Méthode de comparaison: ordre lexicographique sur (nombre de points Elo; nom et prénom du joueur).
+     * - Ordre sur les points Elo: numérique décroissant.
+     * - Ordre sur les nom et prénom: alphabétique croissant.
+     * C'est-à-dire: (nb1; nom1) <= (nb2; nom2) ssi nb1 > nb2 ou ((nb1 = nb2) et (nom1 <= nom2))
+     */
+    public int compareTo (Object j) {
+        int pointsJ = ((Joueur)j).getPoints();
+        if (points>pointsJ) {return 1;}
+        return 0;
+    }
+    
+    public String toString() {return points+" : "+nom+" "+prenom+" - "+nationalite;}
+    
+    public String toText() {return nom+";"+prenom+";"+nationalite+";"+points;}
+}
