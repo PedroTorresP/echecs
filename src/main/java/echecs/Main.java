ec541fbd66e42be5ab86b90af447bf4b05ed36d6
PedroTorres <pedro@heg.com>
2018-04-17 22:20:00 +0200

    Correction Manifest

--- src/main/java/echecs/Main.java
+++ src/main/java/echecs/Main.java
@@ -4,9 +4,10 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("Message test dans le log");
+        //Logger logger = LoggerFactory.getLogger(Main.class);
+        //logger.info("Message test dans le log");
         new vue.FrmMain().setVisible(true);
-        
+        System.out.println("test");
     }
+    
 }
